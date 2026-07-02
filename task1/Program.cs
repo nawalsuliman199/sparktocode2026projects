142b08ecb1ae81f9898d4f34f998dce00fe5cd8f
nawalsuliman199@gmail.com <nawalsuliman199@gmail.com>
2026-07-02 11:37:04 +0400

    add fisrt project

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System.ComponentModel.Design;
 using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
+using System.Xml.Linq;
 
 namespace task1
 {
@@ -18,88 +20,120 @@ namespace task1
             //Console.WriteLine("Student  :" + isStudent);
             ////////////////////////////////////////////////////////////
 
-            //task 2
-            Console.Write("Enter the length  rectangle ");
-            Double length = double.Parse(Console.ReadLine());
-            Console.Write("Enter the width  rectangle ");
-            Double width = double.Parse(Console.ReadLine());
-            double Area = length * width;
-            double Perimeter = 2 * (length + width);
-            Console.WriteLine("Area :" + Area);
-            Console.WriteLine("Perimeter :" + Perimeter);
+            ////task 2
+            //Console.Write("Enter the length  rectangle ");
+            //Double length = double.Parse(Console.ReadLine());
+            //Console.Write("Enter the width  rectangle ");
+            //Double width = double.Parse(Console.ReadLine());
+            //double Area = length * width;
+            //double Perimeter = 2 * (length + width);
+            //Console.WriteLine("Area :" + Area);
+            //Console.WriteLine("Perimeter :" + Perimeter);
 
-            //////////////////////
-            //task 3
+            ////////////////////////
+            ////task 3
 
-            Console.Write("Enter a whole number ");
-            int num = int.Parse(Console.ReadLine());
-            if (num % 2 == 0)
-            {
-                Console.WriteLine("it is even number ");
-            }
-            else
-            {
-                Console.WriteLine("it is odd number ");
-            }
+            //Console.Write("Enter a whole number ");
+            //int num = int.Parse(Console.ReadLine());
+            //if (num % 2 == 0)
+            //{
+            //    Console.WriteLine("it is even number ");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("it is odd number ");
+            //}
 
 
-            /////////////////////////////////
-            ///task 4
-            Console.Write("Enter your age: ");
+            ///////////////////////////////////
+            /////task 4
+            //Console.Write("Enter your age: ");
+            //int age = int.Parse(Console.ReadLine());
+
+            //Console.Write("Do you have a valid national ID? (yes/no): ");
+            //string answer = Console.ReadLine();
+
+            //bool hasID = answer == "yes";
+
+            //if (age >= 18 && hasID)
+            //{
+            //    Console.WriteLine("You are eligible to vote.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("You are not eligible to vote.");
+            //}
+            ///////////////////////////////
+            /////task 5
+            //Console.Write("Enter your grade (A, B, C, D, F): ");
+            //char grade = char.Parse(Console.ReadLine());
+
+            //switch (grade)
+            //{
+            //    case 'A':
+            //        Console.WriteLine("Excellent");
+            //        break;
+
+            //    case 'B':
+            //        Console.WriteLine("Very Good");
+            //        break;
+
+            //    case 'C':
+            //        Console.WriteLine("Good");
+            //        break;
+
+            //    case 'D':
+            //        Console.WriteLine("Pass");
+            //        break;
+
+            //    case 'F':
+            //        Console.WriteLine("Fail");
+            //        break;
+
+            //    default:
+            //        Console.WriteLine("Invalid grade");
+            //        break;
+            //}
+            //////////////////////////////////
+            /////task 6
+            //Console.Write("temperature in Celsius");
+            //double Celsius = double.Parse(Console.ReadLine());
+            //double Fahrenheit = (Celsius * 9 / 5) + 32;
+            //Console.WriteLine("Fahrenheit : " + Fahrenheit);
+            //if (Celsius < 10)
+            //{
+            //    Console.WriteLine("COLD");
+            //}
+
+            //else if (Celsius <= 30)
+            //{
+            //    Console.WriteLine("MILD");
+            //}
+            //else 
+            //{
+            //    Console.WriteLine("HOT");
+            //}
+            /////////////////
+            ////task 7
+            ///
+            Console.Write("Enter your age :");
             int age = int.Parse(Console.ReadLine());
-
-            Console.Write("Do you have a valid national ID? (yes/no): ");
-            string answer = Console.ReadLine();
-
-            bool hasID = answer == "yes";
-
-            if (age >= 18 && hasID)
+             if (age == 0 && age ==12 )
             {
-                Console.WriteLine("You are eligible to vote.");
+                Console.WriteLine("Category :   Children ");
+                Console.WriteLine(" Ticket price: 2 OMR");
+
+            }
+            else if (age >= 13 && age <= 59)
+            {
+                Console.WriteLine("Category: Adult");
+                Console.WriteLine("Ticket Price: 5 OMR");
             }
             else
             {
-                Console.WriteLine("You are not eligible to vote.");
+                Console.WriteLine("Category: Senior");
+                Console.WriteLine("Ticket Price: 3 OMR");
             }
-            /////////////////////////////
-            ///task 5
-            Console.Write("Enter your grade (A, B, C, D, F): ");
-            char grade = char.Parse(Console.ReadLine());
-
-            switch (grade)
-            {
-                case 'A':
-                    Console.WriteLine("Excellent");
-                    break;
-
-                case 'B':
-                    Console.WriteLine("Very Good");
-                    break;
-
-                case 'C':
-                    Console.WriteLine("Good");
-                    break;
-
-                case 'D':
-                    Console.WriteLine("Pass");
-                    break;
-
-                case 'F':
-                    Console.WriteLine("Fail");
-                    break;
-
-                default:
-                    Console.WriteLine("Invalid grade");
-                    break;
-            }
-            ////////////////////////////////
-            ///task 6
-            
-
-
-
-
-
 
 
 
@@ -113,4 +147,5 @@ namespace task1
 
         }
     }
+
 
