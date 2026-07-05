f83ce3ff06b6eab8ef94db716e83e05f35657538
nawalsuliman199@gmail.com <nawalsuliman199@gmail.com>
2026-07-06 01:06:25 +0400

    complete task 3 1 and 2 sub tasks

---
+++ task 3/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ///////////////////////////////
+            ///task 1
+            Console.Write("Enter a number x :");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("Enter a number y :");
+            double y = double.Parse(Console.ReadLine());
+            double z = y - x;
+            double result = Math.Abs(z);
+            Console.WriteLine("final positive difference : " + result);
+            ///////////////////////////////////
+            ///task 2
+            Console.Write("Enter a number  :");
+            double num = double.Parse(Console.ReadLine());
+            double res = Math.Pow(num,2);
+            double ress = Math.Sqrt(num);
+            Console.WriteLine("The square : " + res + " The  root square: " + ress);
+
+        }
+    }
+}
