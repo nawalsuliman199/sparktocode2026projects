c3d1cf219bf8b89114b367b7273152aa8906eda7
nawalsuliman199@gmail.com <nawalsuliman199@gmail.com>
2026-07-07 00:25:19 +0400

    task 3

--- task 3/Program.cs
+++ task 3/Program.cs
@@ -55,7 +55,86 @@ namespace task_3
             }
             ///////////////////////
             /////TASK 6 
+            Console.Write("Enter your password : ");
+            String password = Console.ReadLine();
+            if (password.Length >= 8 && !password.ToLower().Contains("passowrd"))
+            {
+                Console.WriteLine("Strong password");
+            }
+            else
+            {
+                Console.WriteLine("weak passwoed");
+                if (password.Length < 8)
+                    Console.WriteLine("less than 8 characters ");
+                if (password.ToLower().Contains("password"))
+                    Console.WriteLine("contains the word 'password'  ");
 
+            }
+            ////////////////////////
+            ///task 7
+            Console.Write("Enter the first name: ");
+            string name1 = Console.ReadLine();
+
+            Console.Write("Enter the second name: ");
+            string name2 = Console.ReadLine();
+
+            name1 = name1.Trim().ToUpper();
+            name2 = name2.Trim().ToUpper();
+
+            if (name1 == name2)
+            {
+                Console.WriteLine("Match");
+            }
+            else
+            {
+                Console.WriteLine("No Match");
+            }
+            //////////////////////
+            ////task9
+            Console.Write("Enter a decimal number: ");
+            double number = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Rounded: " + Math.Round(number));
+            Console.WriteLine("Rounded Up: " + Math.Ceiling(number));
+            Console.WriteLine("Rounded Down: " + Math.Floor(number));
+
+            /////////////////
+            ///task 10
+            Console.Write("Enter a sentence: ");
+            string sentence = Console.ReadLine();
+
+            Console.Write("Enter a word: ");
+            string word = Console.ReadLine();
+
+            if (sentence.Contains(word))
+            {
+                Console.WriteLine("First Position: " + sentence.IndexOf(word));
+                Console.WriteLine("Last Position: " + sentence.LastIndexOf(word));
+            }
+            else
+            {
+                Console.WriteLine("Word not found");
+            }
+            ///////////////////////////////////////////
+            //////task 11
+            ///
+
+            //////////////////
+            ///////////task 12 
+            Console.Write("Enter your birth date (yyyy-MM-dd): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+
+            int age = DateTime.Today.Year - birthDate.Year;
+
+            if (DateTime.Today.Month < birthDate.Month ||
+               (DateTime.Today.Month == birthDate.Month &&
+                DateTime.Today.Day < birthDate.Day))
+            {
+                age--;
+            }
+
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Day of Birth: " + birthDate.DayOfWeek);
 
 
         }
