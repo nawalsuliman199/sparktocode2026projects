5a92850917559508648df182573f54221c3fa6da
nawalsuliman199@gmail.com <nawalsuliman199@gmail.com>
2026-07-09 00:24:39 +0400

    task 2

--- task4/Program.cs
+++ task4/Program.cs
@@ -2,17 +2,31 @@
 {
     internal class Program
     {
+        /// task 1
+      
+        
         static void printwecolme(string name)
         {
             Console.WriteLine("welcome " + name);
                 }
+        ///task 2
+        static int square (int number)
+        {
+            return number * number;
+        }
 
         static void Main(string[] args)
         {
-            Console.Write("Enter your name");
+            Console.Write("Enter your name : ");
             string name = Console.ReadLine();
-
             printwecolme(name);
+            ////task 2
+            Console.Write("Enter a number ");
+            int number =int.Parse(Console.
+                ReadLine());
+            int res = square(number);
+            Console.WriteLine("square = " + res);
+            
         }
     }
 }
