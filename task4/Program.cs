8286fe4dea39f690b89739d3b607f270ed6cd7cb
nawalsuliman199@gmail.com <nawalsuliman199@gmail.com>
2026-07-09 01:40:07 +0400

    task 7

--- task4/Program.cs
+++ task4/Program.cs
@@ -31,21 +31,48 @@
         //task 5
         static bool IsEven(int num)
         {
-            return numb % 2 == 0;
+            return num % 2 == 0;
         }
         //task 6
         static double CalculateArea(double length, double width)
         {
             return length * width;
         }
-
-      
         static double CalculatePerimeter(double length, double width)
         {
             return 2 * (length + width);
         }
+        //task 7
+        static string GetGradeLetter(int score)
+        {
+            if (score >= 90)
+            {
+                return "A";
+            }
+            else if (score >= 80)
+            {
+                return "B";
+            }
+            else if (score >= 70)
+            {
+                return "C";
+            }
+            else if (score >= 60)
+            {
+                return "D";
+            }
+            else
+            {
+                return "F";
+            }
+        }
+        //task 8
+        //task 9 
+        //task 10 
+
         static void Main(string[] args)
         {
+            //task 1
             Console.Write("Enter your name : ");
             string name = Console.ReadLine();
             printwecolme(name);
@@ -90,6 +117,12 @@
             Console.WriteLine("Area = " + area);
             Console.WriteLine("Perimeter = " + perimeter);
 
+            Console.ReadLine();
+            //task 7
+            Console.Write("Enter your score: ");
+            int score = int.Parse(Console.ReadLine());
+            string grade = GetGradeLetter(score);
+            Console.WriteLine("Grade = " + grade);
             Console.ReadLine();
 
         }
