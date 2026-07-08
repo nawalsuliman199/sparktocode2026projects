487e358b462f20a972b3f176f8527d33cbd39d0e
nawalsuliman199@gmail.com <nawalsuliman199@gmail.com>
2026-07-09 00:09:44 +0400

    task 1

---
+++ task4/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace task4
+{
+    internal class Program
+    {
+        static void printwecolme(string name)
+        {
+            Console.WriteLine("welcome " + name);
+                }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name");
+            string name = Console.ReadLine();
+
+            printwecolme(name);
+        }
+    }
+}
