64b5aac05aa8e394b8c162b952a4295a639fa377
nawalsuliman199@gmail.com <nawalsuliman199@gmail.com>
2026-07-11 01:55:42 +0400

    task 1 and 2

---
+++ task _5/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace task__5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 1
+            int[] grades = new int[5];
+            for (int i =0;i<grades.Length;i++)
+            {
+                Console.Write("Enter grade "+(i+1));
+                grades[i]= int.Parse(Console.ReadLine());
+
+            }
+            Console.WriteLine(" Student Grades ");
+            foreach(int grade in grades)
+                {
+                Console.WriteLine(grade);
+            }
+            //task 2
+            List <string> todolist = new List<string>(); 
+            for (int i =0; i<5;i++)
+            {
+                Console.Write("Enter task " + (i + 1));
+                todolist.Add(Console.ReadLine());
+            }
+            Console.WriteLine(" your to do list ");
+            foreach (string task in todolist) { 
+                Console.WriteLine(task);
+            }
+            //task 3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
